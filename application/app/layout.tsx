39b2dd1485fc32cf3e2b0259b64f2bb16eba0caf
Tamal Kundu <tamalkundu2002@gmail.com>
2024-11-14 23:05:22 +0530

    updt

--- application/app/layout.tsx
+++ application/app/layout.tsx
@@ -1,18 +1,32 @@
-
 import {
   ClerkProvider,
   SignInButton,
   SignedIn,
   SignedOut,
-  UserButton
-} from '@clerk/nextjs'
-import './globals.css'
-import PageHeader from '@/components/page-header';
+  UserButton,
+} from "@clerk/nextjs";
+import "./globals.css";
+import PageHeader from "@/components/page-header";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: {
+    default: "Nexx",
+    template: `%s | Nexx`,
+  },
+  description: "Manage Feedbacks Of Your Business Seamlessly",
+  icons: [
+    {
+      url: "/logo.png",
+      href: "/logo.png",
+    },
+  ],
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <ClerkProvider>
@@ -23,5 +37,5 @@ export default function RootLayout({
         </body>
       </html>
     </ClerkProvider>
-  )
+  );
 }
