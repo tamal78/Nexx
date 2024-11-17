07334418e4c6ba2eb98ce08e815a6c52684ded2e
Tamal Kundu <tamalkundu2002@gmail.com>
2024-11-17 19:30:09 +0530

    git code

--- application/app/landing-page/hero.tsx
+++ application/app/landing-page/hero.tsx
@@ -1,9 +1,5 @@
 import { Button } from "@/components/ui/button";
-import {
-  SignUpButton,
-  SignedIn,
-  SignedOut,
-} from '@clerk/nextjs'
+import { SignUpButton, SignedIn, SignedOut } from "@clerk/nextjs";
 import Link from "next/link";
 import { LogIn, Github } from "lucide-react";
 import Image from "next/image";
@@ -14,8 +10,12 @@ const Hero = () => {
       <div className="container mx-auto px-4 mb-24 mt-4 flex flex-col md:flex-row justify-center">
         <div className="flex flex-col max-w-sm justify-center">
           <div className="mb-8">
-            <h1 className="mb-5 text-5xl font-extrabold leading-tight">Collect your feedback seamlessly</h1>
-            <p className="text-gray-500 texl-lg">Easily integrate Nexx and start collecting feedback today.</p>
+            <h1 className="mb-5 text-5xl font-extrabold leading-tight">
+              Collect your feedback seamlessly
+            </h1>
+            <p className="text-gray-500 texl-lg">
+              Easily integrate Nexx and start collecting feedback today.
+            </p>
           </div>
           <div>
             <SignedOut>
@@ -23,9 +23,10 @@ const Hero = () => {
                 <div className="flex gap-3">
                   <Button>
                     <LogIn className="w-4 h-4 mr-2" />
-                    Get Started</Button>
+                    Get Started
+                  </Button>
                   <Button variant="secondary" asChild>
-                    <Link href="https://github.com">
+                    <Link href="https://github.com/tamal78/Nexx">
                       <Github className="w-4 h-4 mr-2" />
                       GitHub
                     </Link>
@@ -35,16 +36,24 @@ const Hero = () => {
             </SignedOut>
             <SignedIn>
               <Button asChild>
-                <Link href="/dashboard">Dashboard</Link></Button>
+                <Link href="/dashboard">Dashboard</Link>
+              </Button>
             </SignedIn>
           </div>
         </div>
         <div className="flex-1 max-w-lg">
-          <Image src={'/demo.gif'} alt="demo" layout={'responsive'} width={155} height={155} unoptimized={true} />
+          <Image
+            src={"/demo.gif"}
+            alt="demo"
+            layout={"responsive"}
+            width={155}
+            height={155}
+            unoptimized={true}
+          />
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
 export default Hero;
